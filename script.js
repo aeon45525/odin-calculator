407af4508c39317e43c233fd0f5220ba2684632b
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-02-10 20:56:48 +0800

    added function calculation only performs one function at a time if user types 5+5 then clicks another operator, perform 5+5 first, then shows 10 + whatever operator user clicked pressing clear removes all data from display and result equal button only runs when user has input two numbers
    now need to:
        round answers with long decimals
        disable input when hit ERROR and reset result
        add keyboard support

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const keys = document.querySelectorAll(".key");
 
 let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let operators = ["*", "/", "-", "+"];
+let result;
 keys.forEach((key) => {
   key.addEventListener("click", () => {
     const value = key.textContent;
@@ -22,16 +23,36 @@ keys.forEach((key) => {
     }
     // operators
     else if (operators.includes(value)) {
-      if (checkOperator(display.textContent, value)) {
+      let parsed = toInt(display.textContent);
+      if (parsed.number.length === 2) {
+        result = calculate(parsed.number, parsed.operator);
+        display.textContent = result + value;
+      } else if (checkOperator(display.textContent, value)) {
         display.textContent += value;
       }
     } else if (value === "=") {
-      // operator goes here
+      // calculation
+      let parsed = toInt(display.textContent);
+      if (parsed.number.length === 2) {
+        result = calculate(parsed.number, parsed.operator);
+        display.textContent = result;
+      }
     } else if (value === "C") {
       display.textContent = "";
+      result = null;
     }
   });
 });
+
+function calculate(parsedNum, parsedOpe) {
+  if (parsedOpe[0] === "+") return parsedNum[0] + parsedNum[1];
+  else if (parsedOpe[0] === "-") return parsedNum[0] - parsedNum[1];
+  else if (parsedOpe[0] === "*") return parsedNum[0] * parsedNum[1];
+  else if (parsedOpe[0] === "/") {
+    if (parsedNum[1] === 0) return "ERROR";
+    else return parsedNum[0] / parsedNum[1];
+  }
+}
 
 function toInt(display) {
   let character = "";
